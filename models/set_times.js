dfc8233ac2c978854c0bb1487e974ec0d66d3063
Jerrorld <jerrorldk@gmail.com>
2022-11-12 21:26:59 -0800

    used migrations to create remaining tables for music tour

---
+++ models/set_times.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class set_times extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  set_times.init({
+    set_time_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'set_times',
+  });
+  return set_times;
+};
